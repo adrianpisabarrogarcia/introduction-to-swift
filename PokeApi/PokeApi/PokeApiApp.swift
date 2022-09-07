becbd58c9825232661f73abd04d84d9fd232c964
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2022-09-07 17:08:27 +0200

    Init PokeApiApp

---
+++ PokeApi/PokeApi/PokeApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeApiApp.swift
+//  PokeApi
+//
+//  Created by Adrián Pisabarro García on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct PokeApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
